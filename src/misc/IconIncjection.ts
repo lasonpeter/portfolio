7030cbf900eb7531ca0d02b562d1814e1bd9bd9a
Piotr Lasoń <123111159+lasonpeter@users.noreply.github.com>
2025-03-21 00:18:21 +0100

    feat: Update UI styles, add SVG support, and enhance project
    Updated background color and image sources for better performance. Added SVG icon support for projects and included icons for specific technologies. Improved layout and descriptions for project cards on the software development page.

---
+++ src/misc/IconIncjection.ts
@@ -0,0 +1,17 @@
+﻿export const SVGIcon = {
+    RAYLIB: '/icons/raylib.svg',
+    ASTRO: '/icons/astro.svg',
+    GITHUB: '/icons/github.svg',
+    CPP: '/icons/cplusplus.svg',
+    CSHARP: '/icons/csharp.svg',
+    JAVA: '/icons/java.svg',
+    PYTHON: '/icons/python.svg',
+    JS: '/icons/js.svg',
+    HTML: '/icons/html.svg',
+    CSS: '/icons/css.svg',
+    SQL: '/icons/sql.svg',
+    UNITY: '/icons/unity.svg',
+    TS: '/icons/typescript.svg',
+    AWS: '/icons/aws.svg',
+}
+
